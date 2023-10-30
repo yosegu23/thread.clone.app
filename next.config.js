e794f6649b69bf637c8ba144f9fdac7d9b3f437b
yosegu23 <36472632+yosegu23@users.noreply.github.com>
2023-10-30 19:52:28 +0700

    ignored typescript errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
     experimental: {
       serverActions: true,
       serverComponentsExternalPackages: ["mongoose"],
@@ -23,9 +26,6 @@ const nextConfig = {
               hostname: "placehold.co",
             }
         ],
-        // typescript: {
-        //   ignoreBuildErrors: true,
-        // }
     }
 }
 
